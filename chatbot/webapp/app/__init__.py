a245e61d41656adbfc3b5139ee547053c44f4f53
OnlineCampus <bbarot@horizon.csueastbay.edu>
2018-11-13 12:52:02 -0800

    build docker image for flask app

--- chatbot/webapp/app/__init__.py
+++ chatbot/webapp/app/__init__.py
@@ -1,7 +1,28 @@
 from flask import Flask
 from config import Config
+import json
+import datetime
+from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+
+class JSONEncoder(json.JSONEncoder):
+    ''' extend json-encoder class'''
+
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
 app = Flask(__name__)
-# app.config.from_object(Config)
 
 app.config.from_object(Config)
+mongo = PyMongo(app)
+
+
 from app import main
+app.json_encoder = JSONEncoder
+
+from app.controllers import *
--- chatbot/webapp/app/main.py
+++ chatbot/webapp/app/main.py
@@ -5,6 +5,11 @@ import os
 import requests
 from config import Config
 import json
+import dialogflow_v2beta1
+
+
+
+
 
 #app = Flask(__name__)
 
@@ -12,9 +17,14 @@ import json
 
 @app.route('/')
 def index():
+
     return render_template('test.html')
 
+@app.route('/positive_feedback' ,methods=['POST'])
+def pos_feedback():
+    print("user is satisfied")
 
+#
 # @app.route('/')
 # def my_form():
 #     return render_template('index.html')
@@ -28,11 +38,24 @@ def index():
 #
 #     return render_template('index.html', question = question)
 
+# def test():
+#     client = dialogflow_v2beta1.AgentsClient()
+#     parent = client.project_path(Config.DIALOGFLOW_PROJECT_ID)
+#     response = client.export_agent(parent)
+#     # print("response", response)
+#     def callback(operation_future):
+#         result = operation_future.result()
+#         #print("result",result)
+#
+#
+#     response.add_done_callback(callback)
+#     print("response", response)
+#     metadata = response.metadata()
+#     print("metadata",metadata)
 
 def detect_intent_texts(project_id, session_id, text, language_code):
         session_client = dialogflow.SessionsClient()
         session = session_client.session_path(project_id, session_id)
-
         if text:
             text_input = dialogflow.types.TextInput(
                 text=text, language_code=language_code)
@@ -50,3 +73,30 @@ def send_message():
     response_text = { "message":  fulfillment_text }
 
     return jsonify(response_text)
+
+def create_intent(project_id, display_name, training_phrases_parts,
+                  message_texts):
+    """Create an intent of the given intent type."""
+    import dialogflow_v2 as dialogflow
+    intents_client = dialogflow.IntentsClient()
+
+    parent = intents_client.project_agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.types.Intent.TrainingPhrase.Part(
+            text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.types.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.types.Intent.Message.Text(text=message_texts)
+    message = dialogflow.types.Intent.Message(text=text)
+
+    intent = dialogflow.types.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message])
+
+    response = intents_client.create_intent(parent, intent)
+
+    print('Intent created: {}'.format(response))
