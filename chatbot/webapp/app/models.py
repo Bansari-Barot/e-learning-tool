3a4289f703dcc0a4a186afe863efe8ca5ab50302
OnlineCampus <bbarot@horizon.csueastbay.edu>
2019-03-26 14:32:27 -0700

    fixed the database for test assessment module

--- chatbot/webapp/app/models.py
+++ chatbot/webapp/app/models.py
@@ -1,11 +1,11 @@
 from pymodm import MongoModel, fields, EmbeddedMongoModel
-from pymongo.write_concern import WriteConcern
+#from pymongo.write_concern import WriteConcern
 #from app import mongo
-from pymodm import connect
-from app import app
+#from pymodm import connect
+#from app import app
 
 
-connect(app.config['MONGO_URI'])
+#connect(app.config['MONGO_URI'])
 
 class Chat(EmbeddedMongoModel):
     question=fields.CharField()
@@ -16,13 +16,33 @@ class Chat_History(EmbeddedMongoModel):
     c_id=fields.CharField()
     chats=fields.EmbeddedDocumentListField(Chat)
 
+class Feedback(EmbeddedMongoModel):
+    question=fields.CharField()
+    answer=fields.CharField()
+    points=fields.IntegerField()
+    suggestion=fields.CharField()
+
+class Quiz_record(EmbeddedMongoModel):
+    datetimestamp=fields.DateTimeField()
+    quiz_id=fields.IntegerField()
+    score=fields.IntegerField()
+    feedbacks=fields.EmbeddedDocumentListField(Feedback)
+
 class Student(EmbeddedMongoModel):
     name=fields.CharField()
     email_id=fields.CharField()
     chat_history=fields.EmbeddedDocumentListField(Chat_History)
     net_id = fields.CharField()
     role=fields.CharField()
+    quiz_result=fields.EmbeddedDocumentListField(Quiz_record)
 
+class Question_answer(EmbeddedMongoModel):
+    question=fields.CharField()
+    answer=fields.CharField()
+
+class Quiz(EmbeddedMongoModel):
+    q_id=fields.IntegerField()
+    qa=fields.EmbeddedDocumentListField(Question_answer)
 
 class Course(MongoModel):
     course_id=fields.CharField()
@@ -30,6 +50,7 @@ class Course(MongoModel):
     textbook=fields.URLField()
     topics=fields.ListField()
     students=fields.EmbeddedDocumentListField(Student)
+    quiz=fields.EmbeddedDocumentListField(Quiz)
 
 class Agent(MongoModel):
     course_id=fields.CharField()
